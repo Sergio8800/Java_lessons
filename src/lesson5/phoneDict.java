a0b1d9e3baba6f0b0c41b80f6e29b8cb669c7189
Sergio8800 <wasp8800@gmail.com>
2023-07-10 21:28:19 +0200

    second commit

--- src/lesson5/phoneDict.java
+++ src/lesson5/phoneDict.java
@@ -1,7 +1,25 @@
 package lesson5;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 public class phoneDict {
     public static void main(String[] args) {
 
     }
-}
+    Map<Character,Character> simpol = new HashMap<>();
+        simpol.put('}','{');
+        simpol.put(')','(');
+        simpol.put(']','[');
+        simpol.put('>','<');
+    Stack<Character> stack = new Stack<>();
+        for (Character ch: strg.toCharArray()
+            ) {if (simpol.containsValue(ch)){
+        stack.add(ch);
+
+    }else if (simpol.containsKey(ch)){
+        if (stack.isEmpty() || stack.pop() != simpol.get(ch));
+    }
+
+    }
